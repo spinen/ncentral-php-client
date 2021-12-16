a3e6a7f04975819a47a0817a0e6bfa065089b4d9
Stephen Finney <stephen.finney@spinen.com>
2021-12-16 13:32:31 -0500

    Add WSDL and soap-client configs

---
+++ config/soap-client.php
@@ -0,0 +1,43 @@
+<?php
+
+use Phpro\SoapClient\CodeGenerator\Assembler;
+use Phpro\SoapClient\CodeGenerator\Rules;
+use Phpro\SoapClient\CodeGenerator\Config\Config;
+use Phpro\SoapClient\Soap\Driver\ExtSoap\ExtSoapOptions;
+use Phpro\SoapClient\Soap\Driver\ExtSoap\ExtSoapEngineFactory;
+
+return Config::create()
+    ->setEngine($engine = ExtSoapEngineFactory::fromOptions(
+        ExtSoapOptions::defaults('ncentral-wsdl.xml', [])
+            ->disableWsdlCache()
+    ))
+    ->setTypeDestination('src/Type')
+    ->setTypeNamespace('Spinen\Nable\Ncentral\Type')
+    ->setClientDestination('src')
+    ->setClientName('NcentralClient')
+    ->setClientNamespace('Spinen\Nable\Ncentral')
+    ->setClassMapDestination('src')
+    ->setClassMapName('NcentralClassmap')
+    ->setClassMapNamespace('Spinen\Nable\Ncentral')
+    ->addRule(new Rules\AssembleRule(new Assembler\GetterAssembler(new Assembler\GetterAssemblerOptions())))
+    ->addRule(new Rules\AssembleRule(new Assembler\ImmutableSetterAssembler(
+        new Assembler\ImmutableSetterAssemblerOptions()
+    )))
+    ->addRule(
+        new Rules\IsRequestRule(
+            $engine->getMetadata(),
+            new Rules\MultiRule([
+                new Rules\AssembleRule(new Assembler\RequestAssembler()),
+                new Rules\AssembleRule(new Assembler\ConstructorAssembler(new Assembler\ConstructorAssemblerOptions())),
+            ])
+        )
+    )
+    ->addRule(
+        new Rules\IsResultRule(
+            $engine->getMetadata(),
+            new Rules\MultiRule([
+                new Rules\AssembleRule(new Assembler\ResultAssembler()),
+            ])
+        )
+    )
+;
